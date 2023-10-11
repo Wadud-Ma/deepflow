e2386b2c27325cb00d6b22a98d6df5f92a33a999
anxuenan001 <anxuenan001@163.com>
2023-10-11 14:58:20 +0800

    Kafka Parser Log filter Produce

--- agent/src/flow_generator/protocol_logs/mq/kafka.rs
+++ agent/src/flow_generator/protocol_logs/mq/kafka.rs
@@ -333,13 +333,13 @@ impl KafkaLog {
         info.client_id = String::from_utf8_lossy(&payload[14..14 + client_id_len]).into_owned();
         info!("kafka payload byteArray: {:?}", payload);
         info!("kafka payload len {:?}", payload.len());
-        if payload.len() > KAFKA_REQ_HEADER_LEN + client_id_len {
+        let req_type = info.get_command();
+        if payload.len() > KAFKA_REQ_HEADER_LEN + client_id_len && req_type == "Produce" {
             let topic_len = read_u16_be(&payload[14 + client_id_len..]) as usize;
             let topic_start = 14 + client_id_len + 2;
             info!("kafka payload topic_len {:?}", topic_len);
             if payload.len() >= topic_start + topic_len{
                 let publish_topic = String::from_utf8_lossy(&payload[topic_start..topic_start + topic_len]).into_owned();
-                let req_type = info.get_command();
                 let other = String::from_utf8_lossy(&payload[14 + client_id_len..]).into_owned();
                 info!("kafka payload request body info {:?} === {:?} === {:?}", req_type, publish_topic, other);
             }
