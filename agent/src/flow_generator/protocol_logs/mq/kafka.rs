0749488820bd88ba912ba06966c79d199051cb39
anxuenan001 <anxuenan001@163.com>
2023-10-16 14:35:39 +0800

    [Agent] bugfix out of range

--- agent/src/flow_generator/protocol_logs/mq/kafka.rs
+++ agent/src/flow_generator/protocol_logs/mq/kafka.rs
@@ -435,7 +435,7 @@ impl KafkaLog {
     // 解析 OffsetCommit request
     fn parse_offset_commit_message(&mut self, payload: &[u8], info: &mut KafkaInfo, start: usize) -> Result<()> {
         let api_version = info.api_version;
-        let req_type = info.get_command();
+        // let req_type = info.get_command();
         match api_version {
             1..=6 => {
                 if payload.len() > start {
@@ -451,27 +451,29 @@ impl KafkaLog {
 
                     // string member_id
                     s_index = e_index + 4;
-                    let member_id_len = read_u16_be(&payload[s_index..]) as usize;
-                    s_index += 2;
-                    e_index= s_index + member_id_len;
-                    // let member_id = String::from_utf8_lossy(&payload[s_index..e_index]).into_owned();
+                    if payload.len() > s_index {
+                        let member_id_len = read_u16_be(&payload[s_index..]) as usize;
+                        s_index += 2;
+                        e_index= s_index + member_id_len;
+                        // let member_id = String::from_utf8_lossy(&payload[s_index..e_index]).into_owned();
 
-                    // 为什么+12 而不是+8 呢?
-                    if api_version >= 2 && api_version <= 4 {
-                        // int64 retention_time_ms
-                        // let retention_time_ms = read_u64_be(&payload[e_index..]);
-                        s_index = e_index + 12;
-                    }else {
-                        s_index = e_index + 4;
-                    }
+                        // 为什么+12 而不是+8 呢?
+                        if api_version >= 2 && api_version <= 4 {
+                            // int64 retention_time_ms
+                            // let retention_time_ms = read_u64_be(&payload[e_index..]);
+                            s_index = e_index + 12;
+                        }else {
+                            s_index = e_index + 4;
+                        }
 
-                    let topic_len = read_u16_be(&payload[s_index..]) as usize;
-                    s_index += 2;
-                    e_index = s_index + topic_len;
-                    let topic_name = String::from_utf8_lossy(&payload[s_index..e_index]).into_owned();
-                    if !topic_name.is_empty() && topic_name.is_ascii() {
-                        info.publish_topic = Some(topic_name);
-                        // info!("Kafka Topic name parsed success. current topic_name: {:?}, current api_key: {:?}, current api_version: {:?}, current payload: {:?}", info.publish_topic, req_type, info.api_version, payload);
+                        let topic_len = read_u16_be(&payload[s_index..]) as usize;
+                        s_index += 2;
+                        e_index = s_index + topic_len;
+                        let topic_name = String::from_utf8_lossy(&payload[s_index..e_index]).into_owned();
+                        if !topic_name.is_empty() && topic_name.is_ascii() {
+                            info.publish_topic = Some(topic_name);
+                            // info!("Kafka Topic name parsed success. current topic_name: {:?}, current api_key: {:?}, current api_version: {:?}, current payload: {:?}", info.publish_topic, req_type, info.api_version, payload);
+                        }
                     }
                 }
             }
@@ -487,7 +489,7 @@ impl KafkaLog {
             let body = &payload[index..];
             let topic_len = read_u16_be(&body[..]) as usize;
             let topic_end_index = (2 + topic_len) as usize;
-            let req_type = info.get_command();
+            // let req_type = info.get_command();
             if topic_len > 0 && body.len() > topic_end_index {
                 // 前两个字节为长度
                 let topic_name_bytes: Vec<u8> = body[2..topic_end_index].to_vec();
